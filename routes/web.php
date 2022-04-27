a3775e3a102b63fb02d4fb53bb22dbebd65c7c75
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-04-26 20:36:27 -0400

    Adding USE BookingController to WEB

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShowRoomsController;
+use App\Http\Controllers\BookingController;
 
 /*
 |--------------------------------------------------------------------------
