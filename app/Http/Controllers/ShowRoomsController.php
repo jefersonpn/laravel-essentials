eafd9bf30276e5acd24dbc4cd576708314bd732e
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-01 14:41:54 -0400

    Filtering the data from the SELECT

--- app/Http/Controllers/ShowRoomsController.php
+++ app/Http/Controllers/ShowRoomsController.php
@@ -14,12 +14,21 @@ class ShowRoomsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, $roomType = null)
     {
-        $rooms = Room::get();
+       if (isset($roomType)) {
+           $rooms = Room::where('room_type_id', $roomType)->get();
+           // If we want to compare to some thing we can use like this.
+           //  $rooms = Room::where('room_type_id', '!=', $roomType)->get();
+
+       }else{
+            $rooms = Room::get();
+       }
+       
+        /*  $rooms = Room::get();
         if ($request->query('id') !== null){
             $rooms = $rooms->where('room_type_id', $request->query('id'));        
-        }
+        } */
         return view('rooms.index', ['rooms'=> $rooms]);
     }
 }
